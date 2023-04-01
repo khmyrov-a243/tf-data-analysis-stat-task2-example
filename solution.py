eb53e9a5969bd4bdc28d8bd0bac3e03e3f0de72a
khmyrov-a243 <128284168+khmyrov-a243@users.noreply.github.com>
2023-04-01 17:05:37 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,17 +1,24 @@
 import pandas as pd
 import numpy as np
 
-from scipy import stats
+from scipy.stats import uniform
 
 
 chat_id = 973327975 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    n = len(x)
-    alpha = 1 - p
-    alpha_star = alpha / 2
-    chi2_left = stats.chi2.ppf(alpha_star, 2 * n)
-    chi2_right = stats.chi2.ppf(1 - alpha_star, 2 * n)
-    b_left = np.max(x) * (1 + (n / chi2_right))**(-1/ n)
-    b_right = np.max(x) * (1 + (n / chi2_left))**(-1/ n)
-    return (b_left, b_right)
+    # Находим квантиль распределения для заданного уровня доверия
+    q = 1 - (1 - p) / 2
+    u = uniform.ppf(q, loc=0.02)
+
+    # Вычисляем точечную оценку для b
+    b_hat = np.max(x)
+
+    # Вычисляем стандартную ошибку
+    se = (b_hat - 0.02) / uniform.ppf(q, loc=0.02, scale=b_hat - 0.02)
+
+    # Вычисляем границы доверительного интервала
+    left = b_hat - se * uniform.ppf(1 - (1 - p) / 2, loc=0, scale=1)
+    right = b_hat - se * uniform.ppf((1 - p) / 2, loc=0, scale=1)
+
+    return left, right
