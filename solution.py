419ed7b208ba04a665bd3f19efeace27be691034
khmyrov-a243 <128284168+khmyrov-a243@users.noreply.github.com>
2023-04-01 17:20:02 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,24 +1,17 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import uniform
+from scipy.stats import norm
 
 
 chat_id = 973327975 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Находим квантиль распределения для заданного уровня доверия
-    q = 1 - (1 - p) / 2
-    u = uniform.ppf(q, loc=0.02)
-
-    # Вычисляем точечную оценку для b
-    b_hat = np.max(x)
-
-    # Вычисляем стандартную ошибку
-    se = (b_hat - 0.02) / uniform.ppf(q, loc=0.02, scale=b_hat - 0.02)
-
-    # Вычисляем границы доверительного интервала
-    left = b_hat - se * uniform.ppf(1 - (1 - p) / 2, loc=0, scale=1)
-    right = b_hat - se * uniform.ppf((1 - p) / 2, loc=0, scale=1)
-
-    return left, right
+alpha = 1 - p
+b_hat = x.max() + 0.02
+n = len(x)
+s = np.sqrt(((x - b_hat + 0.02) ** 2).sum() / (n - 1))
+z = norm.ppf(1 - alpha / 2)
+left = b_hat - z * s / np.sqrt(n)
+right = b_hat + z * s / np.sqrt(n)
+return left, right
