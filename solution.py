d9843c05314cad2a820a98b5a5bb9717dbf95d9b
khmyrov-a243 <128284168+khmyrov-a243@users.noreply.github.com>
2023-04-06 18:22:41 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,11 +7,11 @@ from scipy.stats import norm
 chat_id = 973327975 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-alpha = 1 - p
-b_hat = x.max() + 0.02
-n = len(x)
-s = np.sqrt(((x - b_hat + 0.02) ** 2).sum() / (n - 1))
-z = norm.ppf(1 - alpha / 2)
-left = b_hat - z * s / np.sqrt(n)
-right = b_hat + z * s / np.sqrt(n)
-return left, right
+    alpha = 1 - p
+    b_hat = x.max() + 0.02
+    n = len(x)
+    s = np.sqrt(((x - b_hat + 0.02) ** 2).sum() / (n - 1))
+    z = norm.ppf(1 - alpha / 2)
+    left = b_hat - z * s / np.sqrt(n)
+    right = b_hat + z * s / np.sqrt(n)
+    return left, right
